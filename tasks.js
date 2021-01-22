97ca11bc630efcced721bb83be6c8e5530cc490f
XNuCX <76185209+XNuCX@users.noreply.github.com>
2021-01-23 00:13:56 +0200

    added task

--- tasks.js
+++ tasks.js
@@ -10,3 +10,13 @@ function fruit(fr, grams, price) {
 }
 
 fruit("orange", 2500, 1.8);
+
+function gratest(num1, num2) {
+  for (let i = num2; i >= 1; i--) {
+    if (num1 % i === 0 && num2 % i === 0) {
+      console.log(i);
+      break;
+    }
+  }
+}
+gratest(15, 5);
