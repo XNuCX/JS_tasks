2f3c45c6edbcdd6cd88a5875898869a55b459bae
XNuCX <76185209+XNuCX@users.noreply.github.com>
2021-02-03 18:25:55 +0200

    task added

--- tasks.js
+++ tasks.js
@@ -411,3 +411,43 @@ console.log(rect.width);
 console.log(rect.height);
 console.log(rect.color);
 console.log(rect.calcArea());
+
+function solve() {
+  const obj = {
+    mage: function (name) {
+      return {
+        name: name,
+        health: 100,
+        mana: 100,
+        cast: function (spell) {
+          this.mana--;
+          return console.log(`${this.name} cast ${spell}`);
+        },
+      };
+    },
+    fighter: function (name) {
+      return {
+        name: name,
+        health: 100,
+        stamina: 100,
+        fight: function () {
+          this.stamina--;
+          return console.log(`${this.name} slashes at the foe!`);
+        },
+      };
+    },
+  };
+  return obj;
+}
+let create = solve();
+
+const scorcher = create.mage("Scorcher");
+scorcher.cast("fireball");
+scorcher.cast("thunder");
+scorcher.cast("light");
+
+const scorcher2 = create.fighter("Scorcher 2");
+scorcher2.fight();
+
+console.log(scorcher2.stamina);
+console.log(scorcher.mana);
