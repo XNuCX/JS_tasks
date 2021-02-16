5203733939668fda0281e7b257aab11d2bae2bb3
XNuCX <76185209+XNuCX@users.noreply.github.com>
2021-02-16 17:35:28 +0200

    task added

--- advanced_functions/07. Simple Calculator/app.js
+++ advanced_functions/07. Simple Calculator/app.js
@@ -1,7 +1,27 @@
 function calculator() {
-    // TODO:
+  let obj1 = {
+    init: function (selector1, selector2, resultSelector) {
+      let select1 = document.querySelector(selector1).value;
+      let select2 = document.querySelector(selector2).value;
+
+      let result = document.querySelector(resultSelector);
+      this.add = function () {
+        let select1 = document.querySelector(selector1).value;
+        let select2 = document.querySelector(selector2).value;
+        let result = document.querySelector(resultSelector);
+        result.value = Number(select1) + Number(select2);
+      };
+      this.subtract = function () {
+        let select1 = document.querySelector(selector1).value;
+        let select2 = document.querySelector(selector2).value;
+        let result = document.querySelector(resultSelector);
+        result.value = Number(select1) - Number(select2);
+      };
+    },
+  };
+  return obj1;
+  // TODO:
 }
+const calculate = calculator();
 
-
-
-
+calculate.init("#num1", "#num2", "#result");
