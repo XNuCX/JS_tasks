8caaab19a0b3f9732d28baf9a1d174820fc83128
XNuCX <76185209+XNuCX@users.noreply.github.com>
2021-02-16 18:05:08 +0200

    task added

--- advanced_functions/08. Next Article/script.js
+++ advanced_functions/08. Next Article/script.js
@@ -1,3 +1,14 @@
 function getArticleGenerator(articles) {
-    // TODO
+  function keep() {
+    if (articles.length != 0) {
+      let newArticle = document.createElement("article");
+      document.getElementById("content").appendChild(newArticle);
+      document.querySelector("#content").lastChild.textContent = articles[0];
+      articles.shift();
+    }
+    return;
+  }
+
+  return keep;
+  // TODO
 }
