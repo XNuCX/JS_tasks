b5b9f97f16b9c0726cb2d6b5ba6343e892936694
XNuCX <76185209+XNuCX@users.noreply.github.com>
2021-02-17 23:59:08 +0200

    task added

--- unit_testing/06.Dynamic-Validation/app.js
+++ unit_testing/06.Dynamic-Validation/app.js
@@ -1,3 +1,15 @@
 function validate() {
-    // TODO
+  let emailInput = document.querySelector("#email");
+  emailInput.addEventListener("change", (e) => {
+    // e.preventDefault();
+    console.log("change");
+
+    const pattern = /[a-z]+\@[a-z]+\.[a-z]+/g;
+    if (!(emailInput.value == emailInput.value.match(pattern))) {
+      console.log(true);
+
+      emailInput.setAttribute("class", "error");
+    } else emailInput.removeAttribute("class", "error");
+  });
+  // TODO
 }
